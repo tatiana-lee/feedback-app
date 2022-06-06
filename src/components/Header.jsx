dbe6ce467ad6cf6102a1dc79dd920d492b80284e
satania-lee <rockky.tl@gmail.com>
2022-06-06 14:56:42 +0300

    add header,card,list,form

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header({ text }) {
+    return (
+        <header>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+}
+
+export default Header
